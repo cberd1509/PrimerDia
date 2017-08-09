9e533b35b06c442ed418096ff35bb49fc920597a
CENTIC <CENTIC@10.6.66.97>
2017-08-09 09:35:20 -0500

    Initial check in

---
+++ Archivo1.m
@@ -0,0 +1,10 @@
+data = rand(1000,1); %Genera  1000 Valores de 0 a 1
+minimo = 3; %Minimo sd
+maximo = 5;
+
+data2 = data*(maximo-minimo)+minimo;
+
+plot(data2,'r.')
+hold on 
+plot(data,'b.')
+hold off
