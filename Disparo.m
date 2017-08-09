a8bdf5d5d6465f78101270a924653f95c6a88049
CENTIC <CENTIC@10.6.66.97>
2017-08-09 12:04:46 -0500

    GraficaTiroParabolicoVelocidad

--- Disparo.m
+++ Disparo.m
@@ -1,14 +1,17 @@
 clc, clear all, close all
 
-[x1,y1] = TiroParabolico(30,0:0.01:5,10);
-[x2,y2] = TiroParabolico(45,0:0.01:5,10);
-[x3,y3] = TiroParabolico(60,0:0.01:5,10);
+
+t=0:0.01:5;
+
+[x1,y1] = TiroParabolico(30,t,10);
+[x2,y2] = TiroParabolico(45,t,10);
+[x3,y3] = TiroParabolico(60,t,10);
 
 hold on
 
-plot(x1,y1,'r');
-plot(x2,y2,'b');
-plot(x3,y3,'g');
+plot(x1,y1,'r')
+plot(x2,y2,'b')
+plot(x3,y3,'g')
 
 
 xlabel('x')
@@ -18,11 +21,14 @@ title('Graficas')
 maximo = max([max(y1),max(y2),max(y3)]);
 
 ylim([0,maximo]);
+
 figure
 
 v1 = zeros(length(x1-1));
 
-% for i= 1 : 1 : v1
-%  
-% end
+for i= 1 : 1 : length(v1)-2
+    v1(i)=(y1(i+1)-y1(i))/(t(i+1)-t(i));    
+end
+
+plot(t,v1,'g');
     
